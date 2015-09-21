c406b8f7bfd6d9bddb0190ec580f94f924f883bc
Jevin Sidhu <jevin.sidhu@gmail.com>
2015-09-21 00:30:13 -0400

    First Commit!
    App that gives you short stories to read dependent on your time
    perference

---
+++ Toilet Stories/LengthViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Toilet Stories
+//
+//  Created by Jevin Sidhu on 2015-09-20.
+//  Copyright (c) 2015 Jevin Sidhu. All rights reserved.
+//
+
+import UIKit
+
+class LengthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
