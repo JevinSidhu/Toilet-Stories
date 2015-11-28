6dd32914830f88ccab8a9e419fc6f22235aefd0c
Jevin Sidhu <jevin.sidhu@gmail.com>
2015-11-28 01:36:35 -0500

    restart, hackathon- time

--- Toilet Stories/LengthViewController.swift
+++ Toilet Stories/LengthViewController.swift
@@ -19,7 +19,11 @@ class LengthViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
 
-
+    
 }
 
